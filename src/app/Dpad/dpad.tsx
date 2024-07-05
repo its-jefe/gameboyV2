fd967c646b8b5b6ea2dbddc4a66e39ee2a81812d
Jeff Jobs <Jeff.Timson@Yahoo.com>
2024-07-05 14:58:31 -0400

    State for snake and food seem stable enough for now.
    - Added arrow keyListener to the dpad for testing.
    - The snake and food are generating properly and keeping their positions. collision boundaries are working properly
    - thinking about updating to classes with render functionality

--- src/app/Dpad/dpad.tsx
+++ src/app/Dpad/dpad.tsx
@@ -3,6 +3,30 @@ import styles from "../page.module.css";
 import { MouseEvent } from "react";
 
 export default function DPad({sendDirectionToGameboy}: any) {
+
+  // TODO: 
+  // fix the interchange of mouse and keys. it resets the ui ... niche git
+  // make sure the button pressure effect relates to the active direction when no mouse is over
+
+  document.addEventListener("keydown", (e: KeyboardEvent) => {
+    var keyCode = e.code;
+
+    switch (keyCode) {
+      case "ArrowLeft" /* LEFT */: 
+        handleInput("L");
+      break;
+      case "ArrowUp" /* UP */: 
+        handleInput("U");
+      break;
+      case "ArrowRight" /* RIGHT */: 
+        handleInput("R");
+      break;
+      case "ArrowDown" /* DOWN */:
+        handleInput("D"); 
+      break;
+    }
+  })
+
   let currentDir = "";
   let percentX = 0;
   let percentY = 0;
@@ -38,19 +62,46 @@ export default function DPad({sendDirectionToGameboy}: any) {
       newDir = percentY > 0 ? "D" : "U";
     }
     
+    handleInput(newDir);
+
+    target.style.transform = `rotateX(${-percentY}deg) rotateY(${percentX}deg)`;
+  };
+
+  const handleMouseLeave =  (e: MouseEvent<HTMLDivElement>) => {
+    // ez
+    if (currentDir == "L"){
+      rightButton.style.color = "rgb(105, 105, 105)";
+    }
+    if (currentDir == "R"){
+      leftButton.style.color = "rgb(105, 105, 105)";
+    }
+    if (currentDir == "U"){
+      downButton.style.color = "rgb(105, 105, 105)";
+    }
+    if (currentDir == "D"){
+      upButton.style.color = "rgb(105, 105, 105)";
+    }
+    // let target = e.currentTarget;
+    // leftButton.style.color = "rgb(105, 105, 105)";
+    // upButton.style.color = "rgb(105, 105, 105)";
+    // rightButton.style.color = "rgb(105, 105, 105)";
+    // downButton.style.color = "rgb(105, 105, 105)";  
+  }
+
+  function handleInput (newDir : string) {
     if (!leftButtonObserved){
-      leftButton = target.querySelector(`#dpad_left`) as HTMLDivElement;
+      leftButton = document.querySelector(`#dpad_left`) as HTMLDivElement;
     }
     if (!upButtonObserved){
-      upButton = target.querySelector(`#dpad_up`) as HTMLDivElement;
+      upButton = document.querySelector(`#dpad_up`) as HTMLDivElement;
     }
     if (!rightButtonObserved){
-      rightButton = target.querySelector(`#dpad_right`) as HTMLDivElement;
+      rightButton = document.querySelector(`#dpad_right`) as HTMLDivElement;
     }
     if (!downButtonObserved){
-      downButton = target.querySelector(`#dpad_down`) as HTMLDivElement;
+      downButton = document.querySelector(`#dpad_down`) as HTMLDivElement;
     }
-    
+
     switch (newDir) {
       case "L":
         leftButton.style.color = "rgba(113, 255, 194, 0.5)";
@@ -99,37 +150,10 @@ export default function DPad({sendDirectionToGameboy}: any) {
         upButton.style.color = "rgb(105, 33, 33)";
         return;
       }
+
       currentDir = newDir;
       sendDirectionToGameboy(newDir as string);
     }
-
-    // so if 0 nothing. aka use existing val
-
-    // console.log(percentX, percentY);
-    // console.log(xDir)
-
-    target.style.transform = `rotateX(${-percentY}deg) rotateY(${percentX}deg)`;
-  };
-
-  const handleMouseLeave =  (e: MouseEvent<HTMLDivElement>) => {
-    // ez
-    if (currentDir == "L"){
-      rightButton.style.color = "rgb(105, 105, 105)";
-    }
-    if (currentDir == "R"){
-      leftButton.style.color = "rgb(105, 105, 105)";
-    }
-    if (currentDir == "U"){
-      downButton.style.color = "rgb(105, 105, 105)";
-    }
-    if (currentDir == "D"){
-      upButton.style.color = "rgb(105, 105, 105)";
-    }
-    // let target = e.currentTarget;
-    // leftButton.style.color = "rgb(105, 105, 105)";
-    // upButton.style.color = "rgb(105, 105, 105)";
-    // rightButton.style.color = "rgb(105, 105, 105)";
-    // downButton.style.color = "rgb(105, 105, 105)";  
   }
 
   return (
--- src/app/Gameboy/gameboy.tsx
+++ src/app/Gameboy/gameboy.tsx
@@ -5,25 +5,46 @@ import Dpad from "../Dpad/dpad"
 import styles from "../page.module.css";
 // import { userAgent } from "next/server";
 
-type Screen = {
-  canvasRef: HTMLCanvasElement | null,
-  canvas: HTMLCanvasElement | null,
-  size: number
+// type Screen = {
+//   canvasRef: HTMLCanvasElement | null,
+//   canvas: HTMLCanvasElement | null,
+//   size: number
+// }
+
+// type Snake = {
+//   size: number,
+//   x: number,
+//   y: number,
+//   tail: number,
+//   init: boolean
+// }
+
+class Snake {
+  constructor(public parent : Snake | null = null) {
+    if (parent != null) {
+      this.parent = parent;
+      this.id = parent.id + 1;
+    }
+  }
+  id: number = 0;
+  size: number = 0;
+  x: number = 0;
+  y: number = 0;
+  initialized: boolean = false;
+  init = () => {
+
+  };
+  tail: Snake | null = null;
+  draw = () => {
+
+  };
 }
 
-type Snake = {
-  size: number,
-  x: number,
-  y: number,
-  tail: number
-  init: boolean
-}
-
-type Food = {
-  size: number,
-  x: number,
-  y: number,
-  init: boolean
+class Food {
+  size: number = 0;
+  x: number = 0;
+  y: number = 0;
+  init: boolean = false;
 }
 
 const handleMouseLeave = (e: MouseEvent<HTMLDivElement>) => {
@@ -35,91 +56,48 @@ const handleMouseLeave = (e: MouseEvent<HTMLDivElement>) => {
 // https://stackoverflow.com/questions/74048836/in-javascript-is-there-a-click-and-drag-event-listener-for-mobiles
 
 export default function Gameboy() {
-  const handleResize = useCallback(
-    (e: Event) => {
-      sizeCanvas(); // dont want this to reset x.y values on the snake
 
-      if (!context || !squareCanvasSize || !snakeHeadSize) { return }
+  /* // Resize functionality 
+  // const handleResize = useCallback(
+  //   (e: Event) => {
+  //     sizeCanvas(); // dont want this to reset x.y values on the snake
 
-      context.fillStyle = "blue";
+  //     if (!context || !squareCanvasSize || !snake.size) { return }
 
-      context.fillRect(pos.x, pos.y, snakeHeadSize, snakeHeadSize);
-    }, [])
+  //     context.fillStyle = "blue";
 
-  if (typeof window !== "undefined") {
-    window.addEventListener('resize', handleResize);
-  }
+  //     context.fillRect(snake.x, snake.y, snake.size, snake.size);
+  //   }, [])
+
+  // if (typeof window !== "undefined") {
+  //   window.addEventListener('resize', handleResize);
+  // }
+  */
+
+  useEffect(() => {
+    // set canvas context and size
+    sizeCanvas();
+    init();
+  },);
 
   const canvasRef = useRef<HTMLCanvasElement | null>(null);
 
   const [canvas, setCanvas] = useState<HTMLCanvasElement | null>(null);
   const [context, setContext] = useState<CanvasRenderingContext2D | null>(null);
   const [squareCanvasSize, setSquareCanvasSize] = useState<number | null>(null)
-
-  const [snakeHeadSize, setSnakeHeadSize] = useState<number | null>(null)
-  // const [food, setFood] = useState
-
   const [start, setStart] = useState<boolean>(false);
 
+  // TODO: I dont think i need the inits 
+  const [snake, setSnake] = useState<Snake>(new Snake());
+  const [food, setFood] = useState<Food>(new Food());
+
   let minRender: number = 0;
-  let maxRender: number | null = (squareCanvasSize != null && snakeHeadSize != null) ? squareCanvasSize - snakeHeadSize : null;
-
-  const [pos, setPos] = useState({
-    init: false,
-    x: 0,
-    y: 0
-  })
-
-  const snake: Snake = {
-    size: 0,
-    x: 0,
-    y: 0,
-    init: false
-  }
-
-  const food: Food = {
-    size: 0,
-    x: 0,
-    y: 0,
-    init: false
-  }
-
+  let maxRender: number | null = (squareCanvasSize != null && snake.size != null) ? squareCanvasSize - snake.size : null;
   let direction: string | null = null;
-
-  useEffect(() => {
-    // set canvas context and size
-
-    debugger;
-
-    sizeCanvas();
-
-
-    init();
-
-
-  },);
-
-  function init() {
-
-    if (!context || !squareCanvasSize || !snakeHeadSize) { return }
-
-    if (pos.init == false) {
-      setPos({
-        init: true,
-        x: (maxRender != null && minRender != null) ? Math.random() * (maxRender - minRender) + minRender : 0,
-        y: (maxRender != null && minRender != null) ? Math.random() * (maxRender - minRender) + minRender : 0
-      });
-    }
-    context.fillStyle = "blue";
-    context.fillRect(pos.x, pos.y, snakeHeadSize, snakeHeadSize);
-
-    food.size = snakeHeadSize * .75;
-    context.fillStyle = "red";
-    context.fillRect(food.x, food.y, food.size, food.size);
-  }
 
   function sizeCanvas() {
     var canvasContainer = document.getElementById("Screen_Nest");
+
     if (!canvasContainer) {
       return;
     }
@@ -128,10 +106,10 @@ export default function Gameboy() {
     var h = canvasContainer.clientHeight;
     var w = canvasContainer.clientWidth;
 
-    var average = h * 2 / 2;
+    var screenAverage = h * 2 / 2;
 
-    setSquareCanvasSize(average);
-    setSnakeHeadSize(average / 15);
+    snake.size = (screenAverage * .06);
+    setSquareCanvasSize(screenAverage);
 
     //i.e. value other than null or undefined
     if (canvasRef.current) {
@@ -143,8 +121,146 @@ export default function Gameboy() {
     }
   }
 
-  function updateDirection(data: string) {
+  function init() {
+    animate();
+  }
 
+  function updateSnake(x: number, y: number) {
+    if (!context || !squareCanvasSize || !snake.size) { return }
+
+    snake.size = snake.size;
+
+    if (start) {
+      snake.x = ((((snake.x + (x)) % squareCanvasSize) + squareCanvasSize) % squareCanvasSize);
+      snake.y = ((((snake.y + (y)) % squareCanvasSize) + squareCanvasSize) % squareCanvasSize);
+
+      // if i made the tail of type snake . and each snake had its own tail . 
+      // I could probably run a nice chain that way
+      // so each snake would manage its own tail .. etc etc .. 
+      if (snake.tail != null) {
+        drawTail();
+      }
+    }
+    else if (!snake.init) {
+      // init
+      if (maxRender != null && minRender != null) {
+        setSnake({
+          id: 0,
+          parent: null,
+          size: snake.size,
+          x: Math.random() * (maxRender - minRender) + minRender,
+          y: Math.random() * (maxRender - minRender) + minRender,
+          initialized: true,
+          tail: null,
+          init(){},
+          draw(){}
+        })
+      }
+    }
+
+    context.fillStyle = "blue";
+    context.fillRect(snake.x, snake.y, snake.size, snake.size);
+
+    let yPeek = false;
+    let xPeek = false;
+
+    if (snake.x > squareCanvasSize - snake.size) {
+      xPeek = true;
+      // context.fillStyle = "purple";
+      context.fillRect(0 - (squareCanvasSize - snake.x), snake.y, snake.size, snake.size);
+    }
+
+    if (snake.y > squareCanvasSize - snake.size) {
+      yPeek = true;
+      // context.fillStyle = "purple";
+      context.fillRect(snake.x, 0 - (squareCanvasSize - snake.y), snake.size, snake.size);
+    }
+
+    if (xPeek && yPeek) {
+      // need a 3rd extra renderer to fill the missing diagonal corner...
+      // will this always be the 0,0 corder? I think so ...
+      // context.fillStyle = "purple";
+      context.fillRect(0 - (squareCanvasSize - snake.x), 0 - (squareCanvasSize - snake.y), snake.size, snake.size);
+    }
+  }
+
+  function updateFood() {
+    if (!context || !squareCanvasSize || !snake.size) { return }
+
+    // snake position should be called before food position..
+    // If the snsake moves into the food position ... 
+    // then need to recalc food position ...
+
+    // HERE : Check snake position 
+    if (!food.init) {
+      // init
+      setFood({
+        size: snake.size * .75,
+        x: (maxRender != null && minRender != null) ? Math.random() * (maxRender - minRender) + minRender : 0,
+        y: (maxRender != null && minRender != null) ? Math.random() * (maxRender - minRender) + minRender : 0,
+        init: true
+      });
+    }
+    else { // CALC THE BOUNDS
+      var foodBounds = {
+        top: food.y,
+        bottom: food.y + food.size,
+        left: food.x,
+        right: food.x + food.size
+      };
+
+      var snakeBounds = {
+        top: snake.y,
+        bottom: snake.y + snake.size,
+        left: snake.x,
+        right: snake.x + snake.size
+      }
+
+      var withinX = (snakeBounds.left <= foodBounds.right && snakeBounds.right >= foodBounds.left)
+      var withinY = (snakeBounds.top <= foodBounds.bottom && snakeBounds.bottom >= foodBounds.top)
+
+      if (withinX && withinY) {
+        snake.tail = new Snake;
+
+        // on snake collision 
+        food.size = snake.size * .75;
+        food.x = (maxRender != null && minRender != null) ? Math.random() * (maxRender - minRender) + minRender : 0;
+        food.y = (maxRender != null && minRender != null) ? Math.random() * (maxRender - minRender) + minRender : 0;
+      }
+    }
+
+    context.fillStyle = "red";
+    context.fillRect(food.x, food.y, food.size, food.size);
+  }
+
+  function drawTail() {
+    if (!context || !squareCanvasSize || !snake.size) { return }
+
+    var tailX = 0;
+    var tailY = 0;
+
+    if (direction == "U") {
+      tailY = snake.y + snake.size;
+      tailX = snake.x;
+    }
+    if (direction == "D") {
+      tailY = snake.y - snake.size;
+      tailX = snake.x;
+    }
+    if (direction == "L") {
+      tailX = snake.x + snake.size;
+      tailY = snake.y;
+    }
+    if (direction == "R") {
+      tailX = snake.x - snake.size;
+      tailY = snake.y;
+    }
+
+    context.fillStyle = "grey";
+    context.fillRect(tailX, tailY, snake.size, snake.size);
+  }
+
+  function updateDirection(data: string) {
     setStart(true);
 
     if (!direction) {
@@ -164,16 +280,14 @@ export default function Gameboy() {
   }
 
   function animate() {
-    if (start == false) { return };
-
-    if (!context || !squareCanvasSize || !snakeHeadSize) {
+    if (!context || !squareCanvasSize || !snake.size) {
       return;
     }
 
     var x = 0;
     var y = 0;
 
-    var modifier = 150;
+    var modifier = 300;
 
     switch (direction) {
       case "L":
@@ -191,35 +305,13 @@ export default function Gameboy() {
     }
 
     context.clearRect(0, 0, squareCanvasSize, squareCanvasSize);
-    pos.x = ((((pos.x + x) % squareCanvasSize) + squareCanvasSize) % squareCanvasSize);
-    pos.y = ((((pos.y + y) % squareCanvasSize) + squareCanvasSize) % squareCanvasSize);
 
-    context.fillStyle = "blue";
-    context.fillRect(pos.x, pos.y, snakeHeadSize, snakeHeadSize);
+    updateSnake(x, y);
+    updateFood();
 
-    let yPeek = false;
-    let xPeek = false;
-
-    if (pos.x > squareCanvasSize - snakeHeadSize) {
-      xPeek = true;
-      // context.fillStyle = "purple";
-      context.fillRect(0 - (squareCanvasSize - pos.x), pos.y, snakeHeadSize, snakeHeadSize);
+    if (start) {
+      requestAnimationFrame(animate);
     }
-
-    if (pos.y > squareCanvasSize - snakeHeadSize) {
-      yPeek = true;
-      // context.fillStyle = "purple";
-      context.fillRect(pos.x, 0 - (squareCanvasSize - pos.y), snakeHeadSize, snakeHeadSize);
-    }
-
-    if (xPeek && yPeek) {
-      // need a 3rd extra renderer to fill the missing diagonal corner...
-      // will this always be the 0,0 corder? I think so ...
-      // context.fillStyle = "purple";
-      context.fillRect(0 - (squareCanvasSize - pos.x), 0 - (squareCanvasSize - pos.y), snakeHeadSize, snakeHeadSize);
-    }
-
-    requestAnimationFrame(animate);
   }
 
   return (
